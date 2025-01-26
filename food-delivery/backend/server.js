db360e1aeaef13e2b5e8d6d5e7e09b85f48d3864
darkoiliosky <ilioskydarko@gmail.com>
2025-01-26 02:07:42 +0100

    .

--- food-delivery/backend/server.js
+++ food-delivery/backend/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 // CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"], // Дозволете ги двата порта
   })
 );
 
